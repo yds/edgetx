852a722e1a06a1197da0b51c6374e5920967a30f
gagarinlg <malte@langermann.net>
2022-03-21 15:39:19 +0100

    removed SDCARD if defs

--- radio/src/opentx.cpp
+++ radio/src/opentx.cpp
@@ -39,9 +39,7 @@
 RadioData  g_eeGeneral;
 ModelData  g_model;
 
-#if defined(SDCARD)
 Clipboard clipboard;
-#endif
 
 GlobalData globalData;
 
@@ -237,9 +235,7 @@ void per10ms()
   if (mixWarning & 4) if(((g_tmr10ms&0xFF)==128) || ((g_tmr10ms&0xFF)==136) || ((g_tmr10ms&0xFF)==144)) AUDIO_MIX_WARNING(3);
 #endif
 
-#if defined(SDCARD)
   sdPoll10ms();
-#endif
 
   outputTelemetryBuffer.per10ms();
 
@@ -1369,9 +1365,7 @@ void opentxClose(uint8_t shutdown)
 #endif
   }
 
-#if defined(SDCARD)
   logsClose();
-#endif
 
   storageFlushCurrentModel();
 
@@ -1406,9 +1400,7 @@ void opentxClose(uint8_t shutdown)
 #endif
 #endif
 
-#if defined(SDCARD)
   sdDone();
-#endif
   VirtualFS::instance().stop();
 }
 
@@ -1691,8 +1683,7 @@ void opentxInit()
   SET_POWER_REASON(0);
 #endif
 
-#if defined(SDCARD)
-  // SDCARD related stuff, only done if not unexpectedShutdown
+  // storage related stuff, only done if not unexpectedShutdown
   if (!globalData.unexpectedShutdown) {
 
     if (!sdMounted())
@@ -1729,7 +1720,6 @@ void opentxInit()
 
     logsInit();
   }
-#endif
 
 #if defined(EEPROM)
   if (!radioSettingsValid)
